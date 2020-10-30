cfd7703644d94332bd641da39f8eb9d138d5f3c3
viamarinha <63912351+viamarinha@users.noreply.github.com>
2020-10-29 22:22:51 -0400

    Add a main class

---
+++ Closest number to a zero/src/api/Main.java
@@ -0,0 +1,27 @@
+package api;
+
+import java.util.Arrays;
+import  static java.lang.Math.abs;
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		int[] toto = getAnumbers();
+		
+	Arrays.stream(toto).filter(a -> a !=0)
+	.reduce((a, b)-> abs(a) > abs(b)? b : abs(a) == abs(b)? Math.min(a, b) : a)
+	.ifPresent(System.out::println);
+		
+
+	}
+
+	private static int[] getAnumbers() {
+		
+		 int arr[] = {3, 2, -8, 5,-2, 4}; 
+		 return arr;
+		
+	}
+
+}
